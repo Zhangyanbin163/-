5ba86ee41971fdbb303f5890f30eac688d511d5e
Hello <70867467+Zhangyanbin163@users.noreply.github.com>
2020-09-15 17:23:55 +0800

    单元测试

---
+++ testmaopaoTest.java
@@ -0,0 +1,31 @@
+package zhangyanbin;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class testmaopaoTest {
+
+	@Before
+		  public static void main(String[] args) {
+		  int a[] = { 6, 18, 333, 7, 2, 4, 444, 999, 687 };
+		  int i, j, n, temp;
+		  n = a.length;
+		  for (j = 0; j < n; j++) {
+		  for (i = 0; i < n - j; i++) {
+		  try {
+		  if (a[i] > a[i + 1]) {
+		  temp = a[i];
+		  a[i] = a[i + 1];
+		  a[i + 1] = temp;
+		  }
+		  } catch (Exception e) {  
+		  }
+		  }
+		  }
+		  for (i = 0; i < n; i++) {
+		  System.out.print(a[i] + " ");
+		  }
+		  }
+		  }
